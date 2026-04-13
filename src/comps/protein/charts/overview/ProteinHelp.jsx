8dc1bc07fc3544ca1323e5e5154b885ca6b22187
hnolCol <h.nolte@uni-koeln.de>
2026-04-13 12:01:54 +0200

    adding_math_support

--- src/comps/protein/charts/overview/ProteinHelp.jsx
+++ src/comps/protein/charts/overview/ProteinHelp.jsx
@@ -1,13 +1,11 @@
 import hooks from "@mitocube/api-hooks"
 import Markdown from "react-markdown"
 import remarkGfm from "remark-gfm"
-
+import remarkMath from "remark-math"
 
 export function ProteinHelp() {
-  
+    
     const { data : mk_stat_info, isSuccess } = hooks.info.useGetStatisticInfo()
 
-    return isSuccess ? <div style={{padding: "1rem", maxWidth : "60rem", textAlign : "left"}}><Markdown remarkPlugins={[remarkGfm]}>{mk_stat_info}</Markdown></div> : null
-remarkPlugins={[remarkGfm, remarkMath]}
-rehypePlugins={[rehypeKatex]}
+    return isSuccess ? <div style={{padding: "1rem", maxWidth : "60rem", textAlign : "left"}}><Markdown remarkPlugins={[remarkGfm, remarkMath]}>{mk_stat_info}</Markdown></div> : null
 }
